5fd2ccb6cb440c756a34245ab82ee8bc5a5722db
Josh Hong <19742491+Joshh09@users.noreply.github.com>
2024-06-11 22:44:19 +0000

    renamed docs folder back to public

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const wikiRoutes = require("./routes/wikis.js");
 
 const app = express();
 
-app.use(express.static("./docs"));
+app.use(express.static("./public"));
 app.use(express.json({limit: '5mb'}));
 
 app.use("/api/wiki", wikiRoutes);
